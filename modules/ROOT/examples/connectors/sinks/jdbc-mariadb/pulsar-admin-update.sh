174436431d2b8f9aebf8b6d5743839de56fe4430
David Dieruf <david@dierufventures.com>
2022-11-20 08:11:58 -0500

    Remove connectors area

--- modules/ROOT/examples/connectors/sinks/jdbc-mariadb/pulsar-admin-update.sh
+++
@@ -1,7 +0,0 @@
-./bin/pulsar-admin sinks update \
-  --sink-type jdbc-mariadb \
-  --name "${SINK_NAME}" \
-  --inputs "persistent://${TENANT}/${NAMESPACE}/${INPUT_TOPIC}" \
-  --tenant "${TENANT}" \
-  --parallelism 2 \
-  --sink-config ''
--- modules/ROOT/examples/connectors/sinks/snowflake/pulsar-admin-create.sh
+++
@@ -1,8 +0,0 @@
-./bin/pulsar-admin sinks create \
-  --sink-type snowflake \
-  --name "${SINK_NAME}" \
-  --inputs "persistent://${TENANT}/${NAMESPACE}/${INPUT_TOPIC}" \
-  --tenant "${TENANT}" \
-  --sink-config '{
-      xxxx
-    }'
